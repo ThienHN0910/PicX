55c775c7df66cbb52fd2178fb34c37adf1a8d1a8
daitb <tranbadai1234@gmail.com>
2025-06-11 18:27:46 +0700

    Add API to get all Orders

---
+++ PicXAPI/Models/OrdersDto.cs
@@ -0,0 +1,21 @@
+﻿namespace PicXAPI.Dtos
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public List<OrderDetailDto> Details { get; set; } = new();
+    }
+
+    public class OrderDetailDto
+    {
+        public string ProductTitle { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; } = string.Empty;
+
+        // Artist info (nested directly or simplified)
+        public string ArtistName { get; set; } = string.Empty;
+
+
+    }
+}
