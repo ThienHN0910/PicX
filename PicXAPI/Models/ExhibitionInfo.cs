7a67001a5797fe1ba1026482eca95c47c0193b49
Hnthpcool6 <hnt.vn.vn@gmail.com>
2025-06-13 21:59:09 +0700

    Feat: Crawl Exhibitions API

---
+++ PicXAPI/Models/ExhibitionInfo.cs
@@ -0,0 +1,14 @@
+namespace PicXAPI.Models
+{
+    public class ExhibitionInfo
+    {
+        public string? Title { get; set; } 
+        public string? GalleryOrMuseum { get; set; } 
+        public string? Date { get; set; } 
+        public string? Location { get; set; } 
+        public string? Url { get; set; } 
+        public string? Description { get; set; } 
+        public string? ImageUrl { get; set; } 
+        public string? SourceApi { get; set; } 
+    }
+}
