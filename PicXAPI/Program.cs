cfb8c0e67d1ad9660d3ce3aa38ad303c1cf3133b
Hnthpcool6 <hnt.vn.vn@gmail.com>
2025-07-28 22:27:03 +0700

    fix cors

--- PicXAPI/Program.cs
+++ PicXAPI/Program.cs
@@ -84,16 +84,13 @@ namespace PicXAPI
                 });
 
             builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowReact", policy =>
-                    policy
-                        .WithOrigins(
-                            "https://picx-client.onrender.com"
-                        )
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials());
-            });
+ {
+     options.AddPolicy("AllowReact", policy =>
+         policy
+             .AllowAnyOrigin() // Cho phép tất cả các origin
+             .AllowAnyHeader() // Cho phép tất cả header
+             .AllowAnyMethod()); // Cho phép tất cả method
+ });
 
             builder.Services.AddControllers().AddNewtonsoftJson();
 
